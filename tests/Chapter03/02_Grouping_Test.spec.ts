9c93bf100528550edd7bcb98021731db97731f0e
annthomas2706 <maryannthomas1996@gmail.com>
2026-05-20 21:19:12 +0530

    Grouping

---
+++ tests/Chapter03/02_Grouping_Test.spec.ts
@@ -0,0 +1,85 @@
+//import playwright mode
+import { test, expect } from "@playwright/test";
+
+test.describe("Smoke Test", async () => {
+    test('Test1', async ({ page }) => {
+
+        await test.step('Navigating to URL', async () => {
+            await page.goto('https://github.com/');
+            await page.getByRole('link', { name: 'Sign in' }).click();
+        });
+
+        await test.step('Entering username and password', async () => {
+            await page.getByRole('textbox', { name: 'Username or email address' }).click();
+            await page.getByRole('textbox', { name: 'Username or email address' }).fill('abc');
+            await page.getByRole('textbox', { name: 'Password' }).click();
+            await page.getByRole('textbox', { name: 'Password' }).fill('abc');
+        });
+        await test.step('Clicking Signin Button ', async () => {
+            await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+        });
+        await test.step('Validating error message', async () => {
+            await expect(page.getByRole('alert')).toContainText('Incorrect username or password.');
+        });
+
+
+
+    })
+})
+
+
+test.describe("Regression Test", async () => {
+    test('Test2', async ({ page }) => {
+
+        await test.step('Navigating to URL', async () => {
+            await page.goto('https://github.com/');
+            await page.getByRole('link', { name: 'Sign in' }).click();
+        });
+
+        await test.step('Entering username and password', async () => {
+            await page.getByRole('textbox', { name: 'Username or email address' }).click();
+            await page.getByRole('textbox', { name: 'Username or email address' }).fill('abc');
+            await page.getByRole('textbox', { name: 'Password' }).click();
+            await page.getByRole('textbox', { name: 'Password' }).fill('abc');
+        });
+        await test.step('Clicking Signin Button ', async () => {
+            await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+        });
+        await test.step('Validating error message', async () => {
+            await expect(page.getByRole('alert')).toContainText('Incorrect username or password.');
+        });
+
+
+
+    })
+
+
+
+
+    test('Test3', async ({ page }) => {
+
+        await test.step('Navigating to URL', async () => {
+            await page.goto('https://github.com/');
+            await page.getByRole('link', { name: 'Sign in' }).click();
+        });
+
+        await test.step('Entering username and password', async () => {
+            await page.getByRole('textbox', { name: 'Username or email address' }).click();
+            await page.getByRole('textbox', { name: 'Username or email address' }).fill('abc');
+            await page.getByRole('textbox', { name: 'Password' }).click();
+            await page.getByRole('textbox', { name: 'Password' }).fill('abc');
+        });
+        await test.step('Clicking Signin Button ', async () => {
+            await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+        });
+        await test.step('Validating error message', async () => {
+            await expect(page.getByRole('alert')).toContainText('Incorrect username or password.');
+        });
+
+
+
+
+    })
+
+})
+
