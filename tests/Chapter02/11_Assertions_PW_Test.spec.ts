c1d93094eb78d2454ea3a10162ae567272a1580c
annthomas2706 <maryannthomas1996@gmail.com>
2026-05-20 18:29:27 +0530

    Assertions

---
+++ tests/Chapter02/11_Assertions_PW_Test.spec.ts
@@ -0,0 +1,67 @@
+import { test, expect } from '@playwright/test';
+
+test('Assertions in playwright', async ({ page }) => {
+
+    // Go to URL
+    await page.goto('https://www.youtube.com/');
+
+
+    // visible, editable, enabled, empty
+    await expect(
+        page.getByPlaceholder('Search', { exact: true }).first()
+    ).toBeVisible();
+
+    await expect(
+        page.getByPlaceholder('Search', { exact: true }).first()
+    ).toBeEditable();
+
+    await expect(
+        page.getByPlaceholder('Search', { exact: true }).first()
+    ).toBeEnabled();
+
+    await expect(
+        page.getByPlaceholder('Search', { exact: true }).first()
+    ).toBeEmpty();
+
+
+
+    // Verify URL, title, text, count
+    await page
+        .getByPlaceholder('Search', { exact: true })
+        .first()
+        .click();
+
+    await page
+        .getByPlaceholder('Search', { exact: true })
+        .first()
+        .fill('playwright by testers talk');
+
+    await page
+        .getByPlaceholder('Search', { exact: true })
+        .first()
+        .press('Enter');
+
+
+    await expect(page).toHaveURL(
+        'https://www.youtube.com/results?search_query=playwright+by+testers+talk'
+    );
+
+    await expect(page).toHaveTitle(
+        'playwright by testers talk - YouTube'
+    );
+
+
+    await expect(
+        page.getByRole('link', { name: 'Playwright by Testers Talk ✅' })
+    ).toHaveText('Playwright by Testers Talk ✅');
+
+    await expect(
+        page.getByRole('link', { name: 'Playwright by Testers Talk ✅' })
+    ).toHaveCount(1);
+
+
+
+    
+
+
+});
