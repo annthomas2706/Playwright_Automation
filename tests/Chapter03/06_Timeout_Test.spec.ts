848b4524ac6d65af73eb870b3e7dfbedd24db391
annthomas2706 <maryannthomas1996@gmail.com>
2026-05-21 23:38:48 +0530

    Browser Context

--- tests/Chapter03/06_Timeout_Test.spec.ts
+++ tests/Chapter03/06_Timeout_Test.spec.ts
@@ -1,11 +1,12 @@
 import { test, expect } from '@playwright/test';
 
 test('Timeout Tests', async ({ page }) => {
-    test.setTimeout(1*60*1000)
+  test.setTimeout(1 * 60 * 1000)
   await page.goto('https://www.youtube.com/');
 
-  await page.getByRole('combobox', { name: 'Search' }).click({timeout:5000});
+  await page.getByRole('combobox', { name: 'Search' }).click({ timeout: 5000 });
   await page.getByRole('combobox', { name: 'Search' }).fill('playwright with testers talk');
   await page.goto('https://www.youtube.com/watch?v=788GvvcfwTY&list=PLUeDIlio4THEXmQxNvKmdDxAVloGTHXMr');
-  await expect(page.getByRole('link', { name: 'Playwright TypeScript by' })).toBeVisible({timeout:5000});
+  await expect(page.getByRole('link', { name: 'Playwright TypeScript by' })).toBeVisible({ timeout: 5000 });
+  await page.waitForTimeout(10000);
 });
