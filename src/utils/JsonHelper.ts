1360a28b9fb71488141b721c44cdcbfadb507157
annthomas2706 <maryannthomas1996@gmail.com>
2026-06-01 21:16:46 +0530

    GET Requests

--- src/utils/JsonHelper.ts
+++ src/utils/JsonHelper.ts
@@ -2,9 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import { TestData } from '../interface/Module1TestData.interface';
 
-/**
- * author Testers Talk
- */
+
 export async function loadTestData() {
     const environment = `${process.env.TEST_EXECUTION_ENV}` || 'qa';
     const directoryPath = path.join(__dirname, `../../test-data/`, environment);
